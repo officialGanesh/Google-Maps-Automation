44e152c990ec15b016c5cab6c928cccb884e5f2d
Ganesh <ganuofficial2795@gmail.com>
2021-05-01 08:51:02 +0530

    places identified

--- main.py
+++ main.py
@@ -18,10 +18,54 @@ driver.get("https://www.google.co.in/maps")
 print(driver.title)
 sleep(3)
 
+# search places
+def search():
+    searhc_box_text = driver.find_element_by_xpath("/html/body/jsl/div[3]/div[9]/div[3]/div[1]/div[1]/div[1]/div[2]/form/div/div[3]/div/input[1]")
+    searhc_box_text.send_keys("mumbai")
+
+
+def search_click():
+    sleep(3)
+    search_click = driver.find_element_by_xpath("/html/body/jsl/div[3]/div[9]/div[3]/div[1]/div[1]/div[1]/div[2]/div[1]/button")
+    search_click.click()
+
+
+def direction():
+    sleep(10)
+    direction = driver.find_element_by_xpath("/html/body/jsl/div[3]/div[9]/div[8]/div/div[1]/div/div/div[4]/div[1]/div/button")
+    direction.click()
+
+
+def starting_point():
+    sleep(5)
+    start = driver.find_element_by_xpath("/html/body/jsl/div[3]/div[9]/div[3]/div[1]/div[2]/div/div[3]/div[1]/div[1]/div[2]/div/div/input")
+    start.clear()
+    start.send_keys("Mumbai")
+    
+  
+
+def end_point():
+    sleep(5)
+    end = driver.find_element_by_xpath("/html/body/jsl/div[3]/div[9]/div[3]/div[1]/div[2]/div/div[3]/div[1]/div[2]/div[2]/div/div/input")
+    end.clear()
+    end.send_keys("New delhi")
+
+
+def search_again():
+    sleep(4)
+    search = driver.find_element_by_xpath("/html/body/jsl/div[3]/div[9]/div[3]/div[1]/div[2]/div/div[3]/div[1]/div[2]/div[2]/button[1]")
+    search.click()
+    sleep(10)
 
 if __name__ == "__main__":
 
     Voice_message()
+    search()
+    search_click()
+    direction()
+    starting_point()
+    end_point()
+    search_again()
 
     driver.quit()
     speak.runAndWait()
